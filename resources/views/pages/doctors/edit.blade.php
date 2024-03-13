dbd0491b0d1876e0191fdeebc8e741ec8d7d4a2c
arioabufaiz <96849751+arioabufaiz@users.noreply.github.com>
2024-03-13 09:56:51 +0700

    tugas11

---
+++ resources/views/pages/doctors/edit.blade.php
@@ -0,0 +1,104 @@
+@extends('layouts.app')
+
+@section('title', 'Edit Doctor')
+
+@push('style')
+    <!-- CSS Libraries -->
+    <link rel="stylesheet" href="{{ asset('library/bootstrap-daterangepicker/daterangepicker.css') }}">
+    <link rel="stylesheet" href="{{ asset('library/bootstrap-colorpicker/dist/css/bootstrap-colorpicker.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('library/select2/dist/css/select2.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('library/selectric/public/selectric.css') }}">
+    <link rel="stylesheet" href="{{ asset('library/bootstrap-timepicker/css/bootstrap-timepicker.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('library/bootstrap-tagsinput/dist/bootstrap-tagsinput.css') }}">
+@endpush
+
+@section('main')
+    <div class="main-content">
+        <section class="section">
+            <div class="section-header">
+                <h1>Advanced Forms</h1>
+                <div class="section-header-breadcrumb">
+                    <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
+                    <div class="breadcrumb-item"><a href="#">Forms</a></div>
+                    <div class="breadcrumb-item">Doctors</div>
+                </div>
+            </div>
+
+            <div class="section-body">
+                <h2 class="section-title">Doctors</h2>
+
+
+
+                <div class="card">
+                    <form action="{{ route('doctors.update', $doctor) }}" method="POST">
+                        @csrf
+                        @method('PUT')
+                        <div class="card-header">
+                            <h4>Input Text</h4>
+                        </div>
+                        <div class="card-body">
+                            <div class="form-group">
+                                <label>Name</label>
+                                <input type="text"
+                                    class="form-control @error('doctor_name')
+                                is-invalid
+                            @enderror"
+                                    name="doctor_name" value="{{ $doctor->doctor_name }}">
+                                @error('doctor_name')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <label>Specialist</label>
+                                <input type="number" class="form-control" name="doctor_specialist"
+                                    value="{{ $doctor->phone }}">
+                            </div>
+                            <div class="form-group">
+                                <label>Phone</label>
+                                <input type="number" class="form-control" name="doctor_phone"
+                                    value="{{ $doctor->phone }}">
+                            </div>
+                            <div class="form-group">
+                                <label>Email</label>
+                                <input type="doctor_email"
+                                    class="form-control @error('doctor_email')
+                                is-invalid
+                            @enderror"
+                                    name="doctor_email" value="{{ $doctor->doctor_email }}">
+                                @error('email')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <label>Foto</label>
+                                <input type="string" class="form-control" name="foto" value="{{ $doctor->phone }}">
+                            </div>
+                            <div class="form-group">
+                                <label>Specialist</label>
+                                <input type="string" class="form-control" name="address" value="{{ $doctor->phone }}">
+                            </div>
+                            <div class="form-group">
+                                <label>SIP</label>
+                                <input type="number" class="form-control" name="sip" value="{{ $doctor->phone }}">
+                            </div>
+
+
+
+                        </div>
+                        <div class="card-footer text-right">
+                            <button class="btn btn-primary">Submit</button>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        </section>
+    </div>
+@endsection
+
+@push('scripts')
+@endpush
