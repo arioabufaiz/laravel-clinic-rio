888a47243d277c2590f8d7c261beb1dec6728e92
arioabufaiz <96849751+arioabufaiz@users.noreply.github.com>
2024-03-18 16:02:56 +0700

    pertemuan5

---
+++ app/Http/Controllers/PatientController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class PatientController extends Controller
+{
+    //index
+    public function index(Request $request)
+    {
+        //get all patients paginated
+        //search patients by nik
+        $patients = DB::table('patients')
+            ->when($request->input('nik'), function ($query, $name) {
+                return $query->where('nik', 'like', '%' . $name . '%');
+            })
+            ->orderBy('id', 'desc')
+            ->paginate(10);
+
+        return view('pages.patients.index', compact('patients'));
+    }
+
+    //show
+    public function show($id)
+    {
+        $doctor = DB::table('patients')->where('id', $id)->first();
+        return view('pages.patients.show', compact('patients'));
+    }
+
+}
